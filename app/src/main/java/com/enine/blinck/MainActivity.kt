ad579c62945da847a67c38a92ebc06bc2577b569
nadeer <nadeersalem02@gmail.com>
2021-10-22 11:47:12 +0200

    initial commit

--- app/src/main/java/com/enine/blinck/MainActivity.kt
+++ app/src/main/java/com/enine/blinck/MainActivity.kt
@@ -2,10 +2,29 @@ package com.enine.blinck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+
+lateinit var regBtn : Button
+lateinit var regEmail : EditText
+lateinit var regPass : EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        regBtn = findViewById(R.id.regBtn)
+        regEmail = findViewById(R.id.emailReg)
+        regPass = findViewById(R.id.passwordReg)
+
+        val email = regEmail.text.toString()
+        val pass = regPass.text.toString()
+
+        Log.d("MainActivity", "email is: $email")
+        Log.d("MainActivity", "password is: $pass")
+
+
     }
 }
